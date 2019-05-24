171140550c6d5909d10550344659c3eb90116e40
Thomas <thomas@craftacademy.se>
2019-05-24 11:02:27 +0200

    adds function to transform metric values to imperial on method change

--- assets/application.js
+++ assets/application.js
@@ -3,18 +3,28 @@ const selectorInput = document.getElementById('method')
 const weightValueInput = document.getElementById('weight')
 const heightValueInput = document.getElementById('height')
 const displayResultElement = document.getElementById('results')
+const KG_TO_POUND_MODIFIER = 2.20462262185
+const CM_TO_INCHES_MODIFIER = 0.0254
 let method = 'metric'
 
 selectorInput.addEventListener('change', () => {
   method = selectorInput.options[selectorInput.selectedIndex].value
-  weightValueInput.value = ''
-  heightValueInput.value = ''
   displayResultElement.innerHTML = ''
   if (method === 'imperial') {
+    if (weightValueInput.value) {
+      let weightInPounds = weightValueInput.value * KG_TO_POUND_MODIFIER
+      let heightInInches = heightValueInput.value / CM_TO_INCHES_MODIFIER
+      weightValueInput.value = weightInPounds.toFixed(2)
+      heightValueInput.value = heightInInches.toFixed(2)
+    }
     weightValueInput.placeholder = weightValueInput.placeholder.replace('kg', 'pounds')
     heightValueInput.placeholder = heightValueInput.placeholder.replace('meters', 'inches')
 
   } else {
+    if (weightValueInput && weightValueInput.value) {
+      let weightInPounds = weightValueInput.value * KG_TO_POUND_MODIFIER
+      weightValueInput.value = weightInPounds
+    }
     weightValueInput.placeholder = weightValueInput.placeholder.replace('pounds', 'kg')
     heightValueInput.placeholder = heightValueInput.placeholder.replace('inches', 'meters')
   }
