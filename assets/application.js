5a0a04e2606b498a2879559639d986c6b25f6ef6
Thomas <thomas@craftacademy.se>
2019-05-24 07:48:29 +0200

    adds bmi message display

--- assets/application.js
+++ assets/application.js
@@ -1,12 +1,24 @@
+const getBmiMessage = (bmiValue) => {
+  if (bmiValue < 18.5) {
+    return "Underweight"
+  } else if (bmiValue > 18.5 && bmiValue < 25) {
+    return "Healthy"
+  } else if (bmiValue > 25 && bmiValue < 30) {
+    return "Overweight"
+  } else if (bmiValue > 30) {
+    return "Oese"
+  }
+}
 
-// document.addEventListener('DOMContentLoaded', () => {
-
-// })
+const calculateMetricBmi = (weightValue, heightValue) => {
+  const value = weightValue / (heightValue * heightValue)
+  return value
+}
 
 const calculateBMI = () => {
   const displayResultElement = document.getElementById('results')
   const weightValue = document.getElementById('weight').value
   const heightValue = document.getElementById('height').value
-  let bmiValue = weightValue / (heightValue * heightValue)
-  displayResultElement.innerHTML = `Your BMI value is ${bmiValue}`
+  let bmiValue = calculateMetricBmi(weightValue, heightValue)
+  displayResultElement.innerHTML = `Your BMI value is ${bmiValue.toFixed(2)} and you are ${getBmiMessage(bmiValue)} `
 }
